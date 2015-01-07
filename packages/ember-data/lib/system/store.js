6101f10ed902c47b1fa56e03cfbcf8bd4a26bad3
Cyril Fluck <github@fluck.fr>
2015-01-06 21:55:48 -0800

    Add all changes present in Iverson.
    - `DS.EmbeddedRecordMixin` is totally customized. I will revert
      the changes and create an equivalent mixin inside Iverson.
    - Just `normalizeRelationships` in the store needs
      to be updated to use `Typeable.detectDynamicType`
    
    Our fork is based on the eb9743441f6b6df4f8d4733a211cc262dd1f339c
    commit from Ember Data.
    
    Remove customization into DS.EmbeddedRecordsMixin
    
    Please use `Addepar.EmbeddedRecordsMixin` instead.
    
    Original code by @igorT and @jcollins1991

--- packages/ember-data/lib/system/store.js
+++ packages/ember-data/lib/system/store.js
@@ -1408,6 +1408,10 @@ DS.Store = Ember.Object.extend({
 });
 
 function normalizeRelationships(store, type, data, record) {
+  if (type.detectDynamicType) {
+    type = type.detectDynamicType(data.type);
+  }
+
   type.eachRelationship(function(key, relationship) {
     // A link (usually a URL) was already provided in
     // normalized form
